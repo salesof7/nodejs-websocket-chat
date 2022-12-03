1b6f26acd0552db63c4784225d83c7fc14781f35
salesof7 <jpsaless2002@gmail.com>
2022-12-03 01:42:45 -0300

    feat: create the mongoose connection

--- src/http.ts
+++ src/http.ts
@@ -2,6 +2,12 @@ import express from "express";
 import path from "node:path";
 import { createServer } from "node:http";
 import { Server } from "socket.io";
+import mongoose from "mongoose";
+
+async function dbConnect(): Promise<void> {
+  await mongoose.connect("mongodb://localhost:27017/rocketsocket");
+}
+dbConnect().catch((error) => console.log(error));
 
 const app = express();
 
