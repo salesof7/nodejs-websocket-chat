64cd1912bc0d6f433ae44bfc7a5c8fa0fc143ac8
salesof7 <jpsaless2002@gmail.com>
2022-12-03 02:01:20 -0300

    feat: create chat room schema

---
+++ src/schemas/ChatRoom.ts
@@ -0,0 +1,23 @@
+import mongoose, { Document, Schema } from "mongoose";
+import { v4 as uuid } from "uuid";
+import { IUser } from "./User";
+
+type IChatRoom = Document & {
+  idUsers: IUser[];
+  idChatRoom: String;
+};
+
+const ChatRoomSchema = new Schema({
+  idUsers: {
+    type: Schema.Types.ObjectId,
+    ref: "Users",
+  },
+  idChatRoom: {
+    type: String,
+    default: uuid(),
+  },
+});
+
+const ChatRoom = mongoose.model<IChatRoom>("ChatRoom", ChatRoomSchema);
+
+export { ChatRoom, IChatRoom };
