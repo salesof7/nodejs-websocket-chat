820c4f6c663a4269fdeed366e49749ce7f6630e4
salesof7 <jpsaless2002@gmail.com>
2022-12-04 14:40:28 -0300

    feat: implement creation of chats between users

--- src/websocket/ChatService.ts
+++ src/websocket/ChatService.ts
@@ -1,7 +1,9 @@
 import { container } from "tsyringe";
 import { io } from "../http";
+import { CreateChatRoomService } from "../services/CreateChatRoomService";
 import { CreateUserService } from "../services/CreateUserService";
 import { GetAllUsersService } from "../services/GetAllUsersService";
+import { GetUserBySocketIdService } from "../services/GetUserBySocketIdService";
 
 io.on("connect", (socket) => {
   socket.on("start", async (data) => {
@@ -16,13 +18,29 @@ io.on("connect", (socket) => {
     });
 
     socket.broadcast.emit("new_users", user);
+  });
 
-    socket.on("get_users", async (callback) => {
-      const getAllUsersService = container.resolve(GetAllUsersService);
+  socket.on("get_users", async (callback) => {
+    const getAllUsersService = container.resolve(GetAllUsersService);
 
-      const users = await getAllUsersService.execute();
+    const users = await getAllUsersService.execute();
 
-      callback(users);
-    });
+    callback(users);
+  });
+
+  socket.on("start_chat", async (data, callback) => {
+    const createChatRoomService = container.resolve(CreateChatRoomService);
+    const getUserBySocketIdService = container.resolve(
+      GetUserBySocketIdService
+    );
+
+    const userLogged = await getUserBySocketIdService.execute(socket.id);
+
+    const room = await createChatRoomService.execute([
+      data.idUser,
+      userLogged._id,
+    ]);
+
+    callback(room);
   });
 });
